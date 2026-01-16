dea3d9e1665e78d3385d5371871804e44efa56f5
meganb2026 <meganb2020@163.com>
2026-01-17 02:43:38 +0800

    feat: transfer and submit

--- assets/js/characters.js
+++ assets/js/characters.js
@@ -8,7 +8,8 @@ const characters = {
 		initialItems: ["crown", "pearl-pass", "money"],
 		goal: "取到证据，或找到当前持有证据的人并毒死",
 		goalKey: "prevent_conspiracy",
-		backstory: "你的王位来路不正，这可千万不能让人知道。当年的知情人已经不多了，仅剩最后一件证据还流落民间。你决定利用这个案件，借机销毁证据。"
+		backstory: "你的王位来路不正，这可千万不能让人知道。当年的知情人已经不多了，仅剩最后一件证据还流落民间。你决定利用这个案件，借机销毁证据。",
+		icon: "👑"
 	},
 	"chengying": {
 		name: "程婴",
@@ -18,7 +19,8 @@ const characters = {
 		initialItems: ["chinese-herbs", "sleeping-pill"],
 		goal: "找到先王的死因",
 		goalKey: "find_cure",
-		backstory: "作为国王的御医，你探遍皇宫一无所获。但是年事已高，恐怕时日不多了。正巧这回国王下令彻查，你要趁着这个机会解开谜团。"
+		backstory: "作为国王的御医，你探遍皇宫一无所获。但是年事已高，恐怕时日不多了。正巧这回国王下令彻查，你要趁着这个机会解开谜团。",
+		icon: "⚕️"
 	},
 	"hefei": {
 		name: "何非",
@@ -28,7 +30,8 @@ const characters = {
 		initialItems: ["diving-equipment", "dice"],
 		goal: "在三天内获取足够的钱还清赌债。",
 		goalKey: "prove_identity",
-		backstory: "克劳狄斯登基时，印了新头像的钱币。人人都知道，你最喜欢克劳狄斯的币了。"
+		backstory: "克劳狄斯登基时，印了新头像的钱币。人人都知道，你最喜欢克劳狄斯的币了。",
+		icon: "🏊"
 	},
 	"lixiang": {
 		name: "李想",
@@ -38,7 +41,8 @@ const characters = {
 		initialItems: ["underground-system-blueprint", "clothes"],
 		goal: "找到尸体不是因排水系统设计有问题造成的死亡案件。",
 		goalKey: "discover_secret",
-		backstory: "作为排水系统的总工程师，要是设计问题造成了工匠死亡，那可是很严重的。你必须找出真相。"
+		backstory: "作为排水系统的总工程师，要是设计问题造成了工匠死亡，那可是很严重的。你必须找出真相。",
+		icon: "🔧"
 	},
 	"wuzhizhe": {
 		name: "吴智哲",
@@ -48,7 +52,8 @@ const characters = {
 		initialItems: ["palace-ground-blueprint", "mentor-notes"],
 		goal: "在三天内解出导师留下的线索并保护秘密",
 		goalKey: "protect_secret",
-		backstory: "你的导师在去世前给你留下了手记，里面有一些神秘的流水图案。你遍寻不着思路，决定趁此机会来皇宫碰碰运气。"
+		backstory: "你的导师在去世前给你留下了手记，里面有一些神秘的流水图案。你遍寻不着思路，决定趁此机会来皇宫碰碰运气。",
+		icon: "📐"
 	},
 	"wangweiguo": {
 		name: "王卫国",
@@ -58,7 +63,8 @@ const characters = {
 		initialItems: ["palace-map", "weekly-report"],
 		goal: "按时交付皇宫修建工程",
 		goalKey: "stop_illegal_transport",
-		backstory: "作为运输队队长，你的团队距离交付还剩三天。请找到修建所需的所有材料，保证皇宫按时交付。"
+		backstory: "作为运输队队长，你的团队距离交付还剩三天。请找到修建所需的所有材料，保证皇宫按时交付。",
+		icon: "🚛"
 	}
 };
 
